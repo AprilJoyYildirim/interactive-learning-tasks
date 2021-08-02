3fd5ffeaeae708bfae4259489a9839f3b66f0ed6
AprilJoyYildirim <aprl.yldrm@gmail.com>
2021-08-02 19:12:56 +0000

    route53 ticket1

---
+++ route53/output.tf
@@ -0,0 +1,11 @@
+output "SomeOutput" {
+  value = <<EOF
+        Zone_id   ${aws_route53_record.www.zone_id}
+        Name      ${aws_route53_record.www.name}
+        
+    EOF
+}
+
+output "records" {
+  value = aws_route53_record.www.records
+}
